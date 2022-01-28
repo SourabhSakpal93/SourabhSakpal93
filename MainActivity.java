880910a3bd985cb2e663af6ea239720ef3b9eac7
Sourabh Dattaram Sakpal <90976821+SourabhSakpal93@users.noreply.github.com>
2022-01-27 21:09:16 -0800

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.unitconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+private Button button2;
+private TextView textView2;
+private EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button2 = findViewById(R.id.button2);
+        textView2 = findViewById(R.id.textView2);
+        editText = findViewById(R.id.editText);    }
+}
